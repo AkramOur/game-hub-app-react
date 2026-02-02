61a53915fea07679f1606a3292c30bcd1a564109
akramour <ourgani.akram@gmail.com>
2026-02-02 13:36:11 +0100

    create custom hook to fetch games

--- src/components/NavBar.tsx
+++ src/components/NavBar.tsx
@@ -1,20 +1,14 @@
-import { HStack, Image } from '@chakra-ui/react'
-import logo from '../assets/logo.webp';
-import ColorModeSwitch from './ColorModeSwitch';
-import SearchInput from './SearchInput';
+import { HStack, Image } from "@chakra-ui/react";
+import logo from "../assets/logo.webp";
+import ColorModeSwitch from "./ColorModeSwitch";
 
-interface Props {
-  onSearch: (searchText: string) => void;
-}
-
-const NavBar = ({ onSearch }: Props) => {
+const NavBar = () => {
   return (
-    <HStack padding='10px'>
-      <Image src={logo} boxSize='60px' />
-      <SearchInput onSearch={onSearch} />
+    <HStack justifyContent="space-between" padding="10px">
+      <Image src={logo} boxSize="60px" />
       <ColorModeSwitch />
     </HStack>
-  )
-}
+  );
+};
 
-export default NavBar
+export default NavBar;
--- src/hooks/useGames.ts
+++ src/hooks/useGames.ts
@@ -1,34 +1,35 @@
-import { GameQuery } from "../App";
-import useData from "./useData";
-import { Genre } from "./useGenres";
-
-export interface Platform {
-  id: number;
-  name: string;
-  slug: string;
-}
+import { useEffect, useState } from "react";
+import apiClient from "../services/api-client";
+import { CanceledError } from "axios";
 
 export interface Game {
   id: number;
   name: string;
-  background_image: string;
-  parent_platforms: { platform: Platform }[];
-  metacritic: number;
-  rating_top: number;
 }
 
-const useGames = (gameQuery: GameQuery) =>
-  useData<Game>(
-    "/games",
-    {
-      params: {
-        genres: gameQuery.genre?.id,
-        platforms: gameQuery.platform?.id,
-        ordering: gameQuery.sortOrder,
-        search: gameQuery.searchText
-      },
-    },
-    [gameQuery]
-  );
+export interface FetchGamesResponse {
+  count: number;
+  results: Game[];
+}
+
+const useGames = () => {
+    const [games, setGames] = useState<Game[]>([]);
+    const [error, setError] = useState("");
+    
+    useEffect(() => {
+      const controller = new AbortController();
+      apiClient
+        .get<FetchGamesResponse>("/games", {signal: controller.signal})
+        .then((res) => setGames(res.data.results))
+        .catch((err) => {
+            if(err instanceof CanceledError ) return;
+            setError(err.message);
+        });
+
+        return () => controller.abort();
+    }, []);
+
+    return [games, setGames, error, setError];
+}
 
 export default useGames;
