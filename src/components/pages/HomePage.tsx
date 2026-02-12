f35d2d70cb363c8eb78f4289d770d45a3f2bbde8
akramour <ourgani.akram@gmail.com>
2026-02-12 20:13:05 +0100

    setup routing

---
+++ src/components/pages/HomePage.tsx
@@ -0,0 +1,43 @@
+import { Grid, Show, GridItem, Flex, Box } from "@chakra-ui/react";
+ import GameGrid from "../GameGrid";
+import GameHeading from "../GameHeading";
+import GenreList from "../GenreList";
+import PlatformSelector from "../PlatformSelector";
+import SortSelector from "../SortSelector";
+
+const HomePage = () => {
+  return (
+    <Grid
+      templateAreas={{
+        base: `"main"`,
+        lg: `"aside main"`,
+      }}
+      templateColumns={{
+        base: "1fr",
+        lg: "250px 1fr",
+      }}
+    >
+      <Show above="lg">
+        <GridItem area="aside" paddingX={5}>
+          <GenreList />
+        </GridItem>
+      </Show>
+      <GridItem area="main">
+        <Box paddingLeft={2}>
+          <GameHeading />
+          <Flex marginBottom={5}>
+            <Box marginRight={5}>
+              <PlatformSelector />
+            </Box>
+
+            <SortSelector />
+          </Flex>
+        </Box>
+
+        <GameGrid />
+      </GridItem>
+    </Grid>
+  );
+};
+
+export default HomePage;
---
+++ src/routes.tsx
@@ -0,0 +1,23 @@
+import { createBrowserRouter } from "react-router-dom";
+import GameDetailPage from "./components/pages/GameDetailPage";
+import HomePage from "./components/pages/HomePage";
+import Layout from "./components/pages/Layout";
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <Layout />,
+    children: [
+      {
+        index: true,
+        element: <HomePage />,
+      },
+      {
+        path: "/games/:id",
+        element: <GameDetailPage />,
+      },
+    ],
+  },
+]);
+
+export default router;
