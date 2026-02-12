c8a3225367fdf7f6c94582f8884db054b89633fe
akramour <ourgani.akram@gmail.com>
2026-02-12 20:20:07 +0100

    handle errors

--- src/routes.tsx
+++ src/routes.tsx
@@ -1,4 +1,5 @@
 import { createBrowserRouter } from "react-router-dom";
+import ErrorPage from "./components/pages/ErrorPage";
 import GameDetailPage from "./components/pages/GameDetailPage";
 import HomePage from "./components/pages/HomePage";
 import Layout from "./components/pages/Layout";
@@ -7,6 +8,7 @@ const router = createBrowserRouter([
   {
     path: "/",
     element: <Layout />,
+    errorElement: <ErrorPage />,
     children: [
       {
         index: true,
